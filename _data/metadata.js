5186be1b79670bafebeac1ca9e1c92f4a5f3f1e0
BG6LH <bg6lh@qsl.net>
2025-05-17 22:16:19 +0800

    meta updated

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,12 +1,12 @@
 export default {
 	title: "BG6LH的数字花园",
-	url: "https://example.com/",
+	url: "https://bg6lh.github.io/",
 	language: "zh",
 	description: "CQ CQ CQ 这里是 BG6LH，欢迎访问我的数字花园！我的业余无线电笔记。",
 	author: {
 		name: "BG6LH",
 		email: "bg6lh@qsl.net",
-		url: "https://bg6lh.github.io/about-me/"
+		url: "https://bg6lh.github.io/"
 	}
 
 }
