169ed5e54eaebda2e86ac6a28b1212b9999d52b4
BG6LH <bg6lh@qsl.net>
2025-05-19 02:40:50 +0800

    refine theme

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,26 +2,28 @@ export default {
 	title: "BG6LH的数字花园",
 	url: "https://bg6lh.github.io/",
 	language: "zh",
-	logo: "/img/me.svg",
 	description: "CQ DE BG6LH K，欢迎访问我的数字花园！我的业余无线电笔记。",
 	author: {
 		name: "BG6LH",
 		email: "bg6lh@qsl.net",
 		url: "https://bg6lh.github.io/"
 	},
-
+	logo: {
+		"src": "/img/me.svg",
+		"alt": "BG6LH"
+	},
 	"hero": {
         "type": "Hero",
         "title": "BG6LH's Digital Garden",
         "text": "CQ DE BG6LH K，欢迎访问我的数字花园！我的业余无线电笔记。",
-        "image": {
-            "src": "",
-            "alt": "Alt text"
-        },
         "buttons": [
             {
                 "lable": "我的博客",
                 "link": "/blog/"
+            },
+            {
+                "lable": "Github",
+                "link": "https://bg6lh.github.io/"
             }
         ],
 		"heroBackgroundImg": "/img/home-bg.jpg",
